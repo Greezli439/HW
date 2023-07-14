acd2a980c3ac81ff97282a2fc1dff03a1ae89a8f
Greezli439 <115574069+Greezli439@users.noreply.github.com>
2023-07-14 18:09:20 +0300

    11

--- HW_10/database/mongodb_connection.py
+++
@@ -1,3 +0,0 @@
-from mongoengine import connect
-
-connect(host='mongodb+srv://greezlistuding:UB1VnEMbXIJSxHph@cluster0.1iemlfa.mongodb.net/?retryWrites=true&w=majority')
--- HW_10/database/part_2/model_mongodb.py
+++
@@ -1,7 +0,0 @@
-from mongoengine import Document
-from mongoengine.fields import StringField, BooleanField
-
-class Consumer(Document):
-    name = StringField()
-    email = StringField()
-    notified = BooleanField()
